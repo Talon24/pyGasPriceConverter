6730d2586bb9724bf6934d2ddde466256e3f56ce
Talon24 <talontalon24@gmail.com>
2017-01-29 16:49:20 +0100

    readme and comments

--- GPC.py
+++ GPC.py
@@ -8,9 +8,9 @@ import re
 from decimal import *
 
 def getUnitFactor(UnitFrom, UnitTo):
-    UnitList ={'l':Decimal(1), 'gal':Decimal(3.785411784) } #Base: Liters
+    UnitList ={'l':1, 'gal':3.785411784 } #Base: Liters
     if UnitFrom in UnitList and UnitTo in UnitList:
-        return 1 / UnitList[UnitFrom] * UnitList[UnitTo]
+        return 1 / Decimal(UnitList[UnitFrom]) * Decimal(UnitList[UnitTo])
     else:
         print('Unsupported unit name!')
         raise Exception('UnitError')
@@ -23,20 +23,26 @@ def getCurrencyFactor(CurrencyFrom, CurrencyTo):
     "KRW","MXN","MYR","NZD","PHP","SGD","THB","ZAR"]
 
     if CurrencyTo in supportedCurrencies and CurrencyFrom in supportedCurrencies:
+        Factor = Decimal(1)
         if CurrencyFrom != CurrencyTo:
             url = "http://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml"
             s = urllib.request.urlopen(url)
-            contentsXML = s.read()
-            xml1 = contentsXML.decode()
-            xml2 = xml1.partition("<Cube>")[2].partition("</Cube>")[0]+ "</Cube>"
-            Factor = Decimal(1)
+            xml1 = s.read().decode() #read the xml
+            xml2 = xml1.partition("<Cube>")[2].partition("</Cube>")[0]+ "</Cube>" #get relevant part
 
+            # Check if xml readable
+            if re.findall("currency",xml2) == []:
+                print("No Connection")
+                raise Exception('ConnectionError')
+
+            #Convert Currency to Base (EUR)
             if CurrencyFrom != 'EUR':
                 line = re.findall("currency=.*" + CurrencyFrom + ".*\'.*\'",xml2)
                 Factor = 1 / Decimal(re.findall("\d*\.\d*", line[0])[0])
             else:
                 Factor = Factor
 
+            #Convert Base to Goal Currency
             if CurrencyTo != 'EUR':
                 line2 = re.findall("currency=.*" + CurrencyTo + ".*\'.*\'",xml2)
                 Factor *= Decimal(re.findall("\d*\.\d*", line2[0])[0])
@@ -48,21 +54,22 @@ def getCurrencyFactor(CurrencyFrom, CurrencyTo):
         raise Exception('CurrencyError')
 def main():
     if len(sys.argv) >= 6:
-        StartValue = Decimal(sys.argv[1].replace(",",".")) #To get both decimal seperators
-        StartCurrency = sys.argv[2].upper()
-        StartUnit = sys.argv[3]
-        GoalCurrency = sys.argv[4].upper()
-        GoalUnit = sys.argv[5]
+        StartValue    = Decimal(sys.argv[1].replace(",",".")) #To get both decimal seperators
+        StartCurrency =         sys.argv[2].upper()
+        StartUnit     =         sys.argv[3].lower()
+        GoalCurrency  =         sys.argv[4].upper()
+        GoalUnit      =         sys.argv[5].lower()
 
         try:
-            UnitFactor = getUnitFactor        (StartUnit, GoalUnit)
+            UnitFactor     = getUnitFactor    (StartUnit, GoalUnit)
             CurrencyFactor = getCurrencyFactor(StartCurrency, GoalCurrency)
         except Exception:
             return 0
 
         GoalValue = StartValue * UnitFactor * CurrencyFactor
 
-        print(str(GoalValue) + " " + GoalCurrency + " per " + str(GoalUnit))
+        print(str(StartValue)     + " " + StartCurrency + "/" + StartUnit + " Converts to")
+        print(str(GoalValue)[0:6] + " " + GoalCurrency +  "/" + GoalUnit)
 
 if 1 == 1:
     main()
--- README.md
+++ README.md
@@ -1,2 +1,63 @@
 # pyGasPriceConverter
 Converts gasoline prices and units
+
+## Usage
+### Syntax
+#### Example code input
+
+./GPC.py 1.25 eur l USD gal
+
+./GPC.py 3.422 usd gal eur l
+
+./GPC.py 1,44 eur l eur gal
+
+#### Mnemonics
+Start like a natural sentence:
+
+convert **Money Amount** from **Currency** per **Unit** to **Currency** per **Unit**
+
+e.g.
+
+convert **1.25** **eur** per **l** to **USD** per **gal**
+
+./GPC.py 1.25 eur l USD gal
+
+### Support
+The allowed Units are
+* Liter "l"
+* Gallon "gal"
+
+The allowed Currencies are
+* EUR
+* USD
+* JPY
+* BGN
+* CZK
+* DKK
+* GBP
+* HUF
+* PLN
+* RON
+* SEK
+* CHF
+* NOK
+* HRK
+* RUB
+* TRY
+* AUD
+* BRL
+* CAD
+* CNY
+* HKD
+* IDR
+* ILS
+* INR
+* KRW
+* MXN
+* MYR
+* NZD
+* PHP
+* SGD
+* THB
+
+The courses are taken from the European Central Bank
